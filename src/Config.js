3c9d53952def160f78f3b23e1795f3214b98ca4c
mazhichao <mayuexiao900@gmail.com>
2018-04-14 15:43:28 +0800

    Feature：封装Book和BookList，初始化加载数据。

---
+++ src/Config.js
@@ -0,0 +1,8 @@
+// Store status in an object so that it can be used and changed easily.
+const BOOK_STATUS = {
+  want: 'wantToRead',
+  reading: 'currentlyReading',
+  read: 'read'
+};
+
+export default BOOK_STATUS;
---
+++ src/components/BookList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Book from './Book';
+
+class BookList extends React.Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired
+  };
+
+  static defaultProps = {
+    books: []
+  };
+
+  render() {
+    const { books } = this.props;
+
+    return (
+      <ol className="books-grid">
+        {books.map(book => (
+          <li key={book.title}>
+            <Book book={book} />
+          </li>
+        ))}
+      </ol>
+    );
+  }
+}
+
+export default BookList;
